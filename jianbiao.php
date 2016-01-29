85c8ae512c421868161df17476293bebd3fa6300
ligy118 <liguanya118@gmail.com>
2016-01-29 20:44:34 +0800

    2ed

---
+++ jianbiao.php
@@ -0,0 +1,29 @@
+<?php
+//$con = mysql_connect("SAE_MYSQL_HOST_M:SAE_MYSQL_PORT","SAE_MYSQL_USER","SAE_MYSQL_PASS");
+//$con = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
+$con = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
+if (!$con)
+{
+    die('Could not connect: ' . mysql_error());
+}
+
+/*if (mysql_query("CREATE DATABASE nmb",$con))
+{
+    echo "Database created";
+}
+else
+{
+    echo "Error creating database: " . mysql_error();
+}*/
+
+mysql_select_db("app_ligy118", $con);
+$sql = "CREATE TABLE neirong
+(
+yonghu text(15),
+time datetime,
+neirong text(1000)
+)";
+mysql_query($sql,$con);
+
+mysql_close($con);
+?>
